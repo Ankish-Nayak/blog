b0c6740a459aef144bc44f1c9ada4f96e65ccf6b
ankish2000nayak@gmail.com <ankish2000nayak@gmail.com>
2023-11-27 00:29:40 +0530

    response object types
    types for response object.

---
+++ packages/types/index.ts
@@ -0,0 +1,63 @@
+export interface IUser {
+  _id: string;
+  name: string;
+  email?: string;
+  posts: string[];
+}
+export interface IReactions {
+  thumbsUp: number;
+  wow: number;
+  heart: number;
+  rocket: number;
+  coffee: number;
+}
+
+export interface IPost {
+  title: string;
+  _id: string;
+  content: string;
+  userId: string;
+  reactions: IReactions;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IgetPosts {
+  posts: IPost[];
+  message: string;
+}
+export interface IgetPost {
+  post: IPost;
+}
+
+export interface IupdatePost {
+  _id: string;
+  message: string;
+}
+
+export interface IcreatePost {
+  _id: string;
+  message: string;
+}
+
+export interface IdeletePost {
+  _id: string;
+  message: string;
+}
+
+export interface IgetUsers {
+  users: IUser[];
+}
+
+export interface IgetUser {
+  user: IUser;
+}
+
+export interface IgetPostsByUserId {
+  posts: IPost[];
+}
+
+export interface IaddReactions {
+  id: string;
+  message: string;
+}
